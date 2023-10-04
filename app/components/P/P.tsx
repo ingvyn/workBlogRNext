251f47244b690dbc0a0396d7e2801d7b66a0d905
Igor Nick <ingvyn@yandex.ru>
2023-10-04 20:35:03 +0300

    add simple components: Htag,Button,P

---
+++ app/components/P/P.tsx
@@ -0,0 +1,18 @@
+import { ParagraphProps } from "./P.props";
+import styles from './P.module.css';
+import cn from 'classnames';
+
+export const P = ({ size = 's', children, className, ...rest }: ParagraphProps): JSX.Element => {
+	return <p
+		className={
+			cn(className, styles.p, {
+				[styles.d]: size == 's',
+				[styles.m]: size == 'm',
+				[styles.l]: size == 'l',
+			}
+			)}
+		{...rest}
+	>
+		{children}
+	</p>;
+};
