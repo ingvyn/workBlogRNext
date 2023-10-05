97c7e5f1898509e17f68f039467a60154acb8d24
Igor Nick <ingvyn@yandex.ru>
2023-10-05 20:37:15 +0300

    add components Card, CardMedia, Likes

---
+++ app/components/Likes/Likes.props.ts
@@ -0,0 +1,5 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+export interface LikesProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+	quantity: number;
+}
