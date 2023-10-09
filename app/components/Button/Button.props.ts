05f7fa88bf1f9a8ea983f6e5b0117fa4e9404ad4
Igor Nick <ingvyn@yandex.ru>
2023-10-09 19:20:18 +0300

    replace quotes in imports

--- app/components/Button/Button.props.ts
+++ app/components/Button/Button.props.ts
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
+import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
 
 export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
 	appearance: 'primary' | 'secondary';
