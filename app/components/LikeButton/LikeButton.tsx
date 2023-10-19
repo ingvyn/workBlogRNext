e066a4eabecbc136b8a44c9598d12ad5471a4866
Igor Nick <ingvyn@yandex.ru>
2023-10-19 22:53:18 +0300

    use getPost method in cleanup function of useEffect

--- app/components/LikeButton/LikeButton.tsx
+++ app/components/LikeButton/LikeButton.tsx
@@ -7,27 +7,31 @@ import { useEffect, useState } from 'react';
 export const LikeButton = ({ className, ...rest }: LikeButtonProps): JSX.Element => {
 	const [liked, setLiked] = useState<boolean>(false);
 	let postData: { userId: number, id: number, title: string, body: string };
+
+	const getPost = async () => {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+			method: 'GET',
+		});
+		postData = await response.json();
+	};
+
+	const patchData = async () => {
+		await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+			method: 'PATCH',
+			body: JSON.stringify(postData),
+			headers: {
+				'Content-type': 'application/json'
+			}
+		});
+	};
+
 	useEffect(() => {
-		const getPost = async () => {
-			const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
-				method: 'GET',
-			});
-			postData = await response.json();
-		};
-		getPost();
-	}, []);
-	useEffect(() => {
-		const patchData = async () => {
-			await fetch('https://jsonplaceholder.typicode.com/posts/1', {
-				method: 'PATCH',
-				body: JSON.stringify(postData),
-				headers: {
-					'Content-type': 'application/json'
-				}
-			});
-		};
 		patchData();
+		return () => {
+			getPost();
+		};
 	}, [liked]);
+
 	return (
 		<button
 			className={cn(styles.common, className, {
