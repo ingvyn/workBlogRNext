cf7f6adbdc31b3f27e5a7637a355127528000f21
Igor Nick <ingvyn@yandex.ru>
2023-11-10 16:59:55 +0300

    pass handler for data transfer to LikeButton through prop

--- app/components/LikeButton/LikeButton.props.ts
+++ app/components/LikeButton/LikeButton.props.ts
@@ -1,4 +1,6 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 
 export interface LikeButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+	post: number;
+	onLike: (post: number) => () => Promise<void>;
 }
