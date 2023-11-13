837a68909c01b277f267f7ee15f83af8dada9d35
Igor Nick <ingvyn@yandex.ru>
2023-11-13 17:11:22 +0300

    combine test Card with children in SampleCard component

---
+++ app/components/SampleCard/SampleCard.tsx
@@ -0,0 +1,42 @@
+import { Card } from '../Card/Card';
+import { Htag } from '../Htag/Htag';
+import { Button } from '../Button/Button';
+import { P } from '../P/P';
+import { Tag } from '../Tag/Tag';
+import { Likes } from '../Likes/Likes';
+import { CardMedia } from '../CardMedia/CardMedia';
+import { CardItems } from '../CardItems/CardItems';
+import { pluralize } from '../../utils/pluralize';
+
+export const SampleCard = (): JSX.Element => {
+	return (
+		<Card>
+			<CardMedia src="cardMedia-Safari (Catalina) - Dark 1.png" />
+			<CardItems lay='col8gap'>
+				<CardItems lay='row'>
+					<CardItems lay='inline'>
+						<Tag>Frontend</Tag>
+						<span>·</span>
+						<Tag>1 месяц назад</Tag>
+					</CardItems>
+					<Likes quantity={4} />
+				</CardItems>
+				<CardItems lay='col0gap'>
+					<Htag tag='h4'>
+						Как работать с CSS Grid
+					</Htag>
+					<P>
+						<span className="underline">Грид-раскладка (CSS Grid Layout)</span> представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.
+					</P>
+				</CardItems>
+			</CardItems>
+			<CardItems lay='row'>
+				<Tag>{`3 ${pluralize(3, ['минута', 'минуты', 'минут'])}`}</Tag>
+				<Button appearance='primary'>
+					Читать
+					<img src="Button-icon-arrow.png" />
+				</Button>
+			</CardItems>
+		</Card>
+	);
+};
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,13 +1,14 @@
 'use client';
-export * from './Card/Card';
+//export * from './Card/Card';
 export * from './Htag/Htag';
-export * from './Button/Button';
-export * from './P/P';
+//export * from './Button/Button';
+//export * from './P/P';
 
 export * from './Header/Header';
-export * from './Tag/Tag';
-export * from './Likes/Likes';
-export * from './CardMedia/CardMedia';
-export * from './CardItems/CardItems';
+//export * from './Tag/Tag';
+//export * from './Likes/Likes';
+//export * from './CardMedia/CardMedia';
+//export * from './CardItems/CardItems';
+export * from './SampleCard/SampleCard';
 export * from './LikeButton/LikeButton';
 export * from './LikeButton/onLike';
