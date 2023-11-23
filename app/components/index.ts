490e602816605b3847082c4208911591569b25b0
Igor Nick <ingvyn@yandex.ru>
2023-11-23 18:27:37 +0300

    shuffled posts by cards

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,7 +2,7 @@
 //export * from './Card/Card';
 export * from './Htag/Htag';
 //export * from './Button/Button';
-//export * from './P/P';
+export * from './P/P';
 
 //export * from './Tag/Tag';
 //export * from './Likes/Likes';
