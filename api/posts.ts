04c040c4c17deebc9355d217b4e86c0ac2387a73
Igor Nick <ingvyn@yandex.ru>
2023-11-26 12:31:01 +0300

    change alias to id and add not existing post handling

--- api/posts.ts
+++ api/posts.ts
@@ -3,10 +3,13 @@ import { PostItem } from '@/interfaces/post.interface';
 export const getSelectedPosts = async (): Promise<PostItem[]> => {
 	const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/posts`);
 	const posts: PostItem[] = await res.json();
-	return posts && posts.filter(({ id }) => id < 10);
+	return posts.filter(({ id }) => id < 10);
 };
 
-export const getPost = async (id: string): Promise<PostItem> => {
+export const getPost = async (id: string): Promise<PostItem | null> => {
 	const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/posts/${id}`);
+	if (!res.ok) {
+		return null;
+	}
 	return res.json();
 };
--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,19 +1,24 @@
 import { getSelectedPosts, getPost } from '@/api/posts';
 import { Htag } from '@/app/components';
 import { PostItem } from '@/interfaces/post.interface';
+import { notFound } from 'next/navigation';
 import { Metadata } from 'next';
 
 export async function generateStaticParams() {
 	const posts: PostItem[] = await getSelectedPosts();
-	return posts.map(({ id }) => ({ alias: id.toString() }));
+	return posts.map(({ id }) => ({ id: id.toString() }));
 }
 
 export const metadata: Metadata = {
 	title: 'Страница поста',
 };
 
-export default async function Post({ params }: { params: { alias: string } }) {
-	const { title, body } = await getPost(params.alias);
+export default async function Post({ params }: { params: { id: string } }) {
+	const post = await getPost(params.id);
+	if (!post) {
+		notFound();
+	}
+	const { title, body } = post;
 	return (
 		<div>
 			<Htag tag='h2'>
