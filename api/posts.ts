ae2056ffa0b67df47453d4e15f204b753fe0e290
Igor Nick <ingvyn@yandex.ru>
2023-12-04 18:04:04 +0300

    change filter on relevant slice in posts forming

--- api/posts.ts
+++ api/posts.ts
@@ -3,7 +3,7 @@ import { PostItem } from '@/interfaces/post.interface';
 export const getSelectedPosts = async (): Promise<PostItem[]> => {
 	const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/posts`);
 	const posts: PostItem[] = await res.json();
-	return posts.filter(({ id }) => id < 10);
+	return posts.slice(0, 9);
 };
 
 export const getPost = async (id: string): Promise<PostItem | null> => {
