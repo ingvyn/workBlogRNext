98d37f5a7f5ff4232c1941e343801ef484c7e95e
Igor Nick <ingvyn@yandex.ru>
2023-12-13 21:34:53 +0300

    add PostComments component and use it on post page

---
+++ app/components/PostComments/PostComments.tsx
@@ -0,0 +1,36 @@
+import { PostCommentsProps } from './PostComments.props';
+import styles from './PostComments.module.css';
+import { Htag } from '..';
+import { CommentItem } from '@/interfaces/comment.interface';
+import cn from 'classnames';
+
+const getComments = async (post: number): Promise<CommentItem[]> => {
+	const data = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${post}`);
+	return data.json();
+};
+
+export const PostComments = async ({ post, className, ...props }: PostCommentsProps): Promise<JSX.Element> => {
+	const comments = await getComments(post);
+	return (
+		<div className={cn(styles.comments,
+			className)}
+			{...props}>
+			<Htag tag='h2'>
+				Комментарии
+			</Htag>
+			{comments && comments.map(({ name, email, body }) => {
+				return <div>
+					<div className={styles.inline}>
+						<span className={styles.name}>{name}</span>
+						<span>·</span>
+						<span className={styles.email}>{email}</span>
+					</div>
+					<div className={styles.comment}>
+						{body}
+					</div>
+				</div>;
+			})
+			}
+		</div>
+	);
+};
