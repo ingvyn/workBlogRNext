5c9a8fcbfa5f91c05d34a2e3b474802985ff299c
Igor Nick <ingvyn@yandex.ru>
2023-12-23 20:04:26 +0300

    on submit add request to imitate sending of comments for patch of selected post

--- api/posts.ts
+++ api/posts.ts
@@ -1,3 +1,4 @@
+import { CommentForm } from '@/interfaces/commentForm.interface';
 import { PostItem } from '@/interfaces/post.interface';
 
 export const getSelectedPosts = async (): Promise<PostItem[]> => {
@@ -8,6 +9,23 @@ export const getSelectedPosts = async (): Promise<PostItem[]> => {
 
 export const getPost = async (id: string): Promise<PostItem | null> => {
 	const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/posts/${id}`);
+	if (!res.ok) {
+		return null;
+	}
+	return res.json();
+};
+
+export const patchPost = async (formData: (CommentForm & { id: string })): Promise<PostItem | null> => {
+	const { id, comment } = formData;
+	const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/posts/${id}`, {
+		method: 'PATCH',
+		body: JSON.stringify({
+			body: comment,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	});
 	if (!res.ok) {
 		return null;
 	}
