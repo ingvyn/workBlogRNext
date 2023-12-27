f9bc55b633d4ec9977734e7b20ba447e3c73477c
Igor Nick <ingvyn@yandex.ru>
2023-12-27 16:03:01 +0300

    set forwardRef typization

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -1,9 +1,9 @@
 import { InputProps } from './Input.props';
 import styles from './Input.module.css';
 import cn from 'classnames';
-import { ForwardedRef, forwardRef } from 'react';
+import { ForwardedRef, forwardRef, memo } from 'react';
 
-export const Input = forwardRef(({ className, error, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
+export const Input = memo(forwardRef<HTMLInputElement, InputProps>(({ className, error, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
 	return (
 		<div className={cn(className, styles.inputWrapper)}>
 			<input className={cn(styles.input, {
@@ -12,4 +12,4 @@ export const Input = forwardRef(({ className, error, ...props }: InputProps, ref
 			{error && <span className={styles.errorMessage}>{error.message}</span>}
 		</div>
 	);
-});
+}));
