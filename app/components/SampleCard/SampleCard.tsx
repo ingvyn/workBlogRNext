265b1938c20ea74b15ef9336188482464468a693
Igor Nick <ingvyn@yandex.ru>
2024-02-04 14:10:35 +0300

    implement keyboard navigation through sample cards of blog grid

--- app/components/SampleCard/SampleCard.tsx
+++ app/components/SampleCard/SampleCard.tsx
@@ -3,38 +3,73 @@ import { Card } from '../Card/Card';
 import { Button } from '../Button/Button';
 import { Tag } from '../Tag/Tag';
 import { Likes } from '../Likes/Likes';
+import { Htag } from '../Htag/Htag';
+import { P } from '../P/P';
 import { CardMedia } from '../CardMedia/CardMedia';
 import { CardItems } from '../CardItems/CardItems';
 import { pluralize } from '../../utils/pluralize';
 import { SampleCardProps } from './SampleCard.props';
 import Link from 'next/dist/client/link';
-import { ForwardedRef, forwardRef } from 'react';
+import { ForwardedRef, forwardRef, useState, KeyboardEvent } from 'react';
 import { motion } from 'framer-motion';
 
-export const SampleCard = motion(forwardRef<HTMLDivElement, SampleCardProps>(({ children, postId, ...props }: SampleCardProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+export const SampleCard = motion(forwardRef<HTMLDivElement, SampleCardProps>(({ postId, title, body, ...props }: SampleCardProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+	const [keyNavigationEnabled, setKeyNavigationEnabled] = useState<boolean>(false);
+	const enableCardNavigation = (key: KeyboardEvent) => {
+		if (key.code == 'Space' || key.code == 'Enter') {
+			key.preventDefault();
+			setKeyNavigationEnabled(true);
+		}
+		if (key.code == 'Tab' && (key.target as Element).id === `lastTabSampleCard${postId}` && key.shiftKey == false) {
+			setKeyNavigationEnabled(false);
+		}
+	};
 	return (
-		<div {...props} ref={ref}>
+		<div {...props} ref={ref}
+			onKeyDown={(event: KeyboardEvent) => enableCardNavigation(event)}
+		>
 			<Card>
-				<CardMedia src="cardMedia-Safari (Catalina) - Dark 1.png" context='list' />
+				<CardMedia src="cardMedia-Safari (Catalina) - Dark 1.png" context='list' tabIndex={keyNavigationEnabled ? 0 : -1} />
 				<CardItems lay='col' className='gap-8'>
-					<CardItems lay='row' className='gap-6' aria-label='Теги' tabIndex={0}>
-						<CardItems id='cardTags' lay='inline' className='gap-6'>
-							<Tag tabIndex={0}>Frontend</Tag>
+					<CardItems id='tagSection' lay='row' className='gap-6' aria-label='Теги'>
+						<CardItems lay='inline' className='gap-6'>
+							<Tag id='tagSectionTag1'
+								tabIndex={keyNavigationEnabled ? 0 : -1}
+								aria-labelledby='tagSection tagSectionTag1'
+							>
+								Frontend
+							</Tag>
 							<span>·</span>
-							<Tag tabIndex={0}>1 месяц назад</Tag>
+							<Tag tabIndex={keyNavigationEnabled ? 0 : -1}>
+								1 месяц назад
+							</Tag>
 						</CardItems>
-						<Likes quantity={4} tabIndex={0} />
+						<Likes quantity={4}
+							tabIndex={keyNavigationEnabled ? 0 : -1} />
 					</CardItems>
 					<CardItems lay='col'>
-						{children}
+						<Htag tag='h4' tabIndex={keyNavigationEnabled ? 0 : -1}>
+							{title}
+						</Htag>
+						<P tabIndex={keyNavigationEnabled ? 0 : -1}>
+							{body}
+						</P>
 					</CardItems>
 				</CardItems>
 				<CardItems id='readSection' lay='row' className='gap-6' aria-label='Для чтения потребуется'>
-					<Tag id='tag1' tabIndex={0} aria-labelledby='readSection tag1'>{`3 ${pluralize(3, ['минута', 'минуты', 'минут'])}`}</Tag>
+					<Tag id='readSectionTag1'
+						tabIndex={keyNavigationEnabled ? 0 : -1}
+						aria-labelledby='readSection readSectionTag1'
+					>
+						{`3 ${pluralize(3, ['минута', 'минуты', 'минут'])}`}
+					</Tag>
 					<Link href={`/post/${postId}`} tabIndex={-1}>
-						<Button appearance='primary'>
+						<Button id={`lastTabSampleCard${postId}`} appearance='primary'
+							tabIndex={keyNavigationEnabled ? 0 : -1}
+							aria-label='Нажмите, чтобы открыть страницу поста'
+						>
 							Читать
-							<img src="Button-icon-arrow.png" />
+							<img src="Button-icon-arrow.png" aria-hidden='true' />
 						</Button>
 					</Link>
 				</CardItems>
